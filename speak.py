50c27271063ffca918bbbb1f4ff81fa316d78274
Bri <briexists@gmail.com>
2026-01-20 14:56:44 -0600

    Change speak.py to use speak and braille separately, since output doesn't seem to do this on it's own for some reason.

--- speak.py
+++ speak.py
@@ -5,4 +5,5 @@ from accessible_output2 import outputs
 speaker = outputs.auto.Auto()
 
 def speak(text, interrupt=False):
-	speaker.output(text, interrupt)
+	speaker.speak(text, interrupt)
+	speaker.braille(text)
